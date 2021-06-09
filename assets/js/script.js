07eb66074accfb759b87cc16b7ce24eed99d7925
Michelle1024 <michelleldqp@yahoo.com>
2021-06-09 17:02:53 +0000

    Create a quiz class and question class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-console.log("Connected!")
+// Create a quiz class
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+    getQuestionIndex() {
+        return this.questions[this.questionIndex];
+    }
+
+    guess(answer) {
+        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+            this.score++;
+        }
+        this.questionIndex++;
+    }
+
+    isEnded() {
+        return this.questionIndex === this.questions.length;
+    }
+}
+
+// Create a question class
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+
